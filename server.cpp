ba8b66e685d9e4a01813694f70bc26022dc81c28
ZeyadAttia5 <85710632+ZeyadAttia5@users.noreply.github.com>
2023-07-25 11:26:23 +0300

    finished parsing the command
    TODO::
        1. do_get
        2. do_set
        3. do_del
        4. convert compare char* with string

--- server.cpp
+++ server.cpp
@@ -11,22 +11,33 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <vector>
+#include <string>
+#include <map>
 
+#define HEADER 4
 
-static void msg(const char *msg) {
+using namespace std;
+
+static map<string, string> g_map;
+
+static void msg(const char *msg)
+{
     fprintf(stderr, "%s\n", msg);
 }
 
-static void die(const char *msg) {
+static void die(const char *msg)
+{
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
 }
 
-static void fd_set_nb(int fd) {
+static void fd_set_nb(int fd)
+{
     errno = 0;
     int flags = fcntl(fd, F_GETFL, 0);
-    if (errno) {
+    if (errno)
+    {
         die("fcntl error");
         return;
     }
@@ -35,53 +46,61 @@ static void fd_set_nb(int fd) {
 
     errno = 0;
     (void)fcntl(fd, F_SETFL, flags);
-    if (errno) {
+    if (errno)
+    {
         die("fcntl error");
     }
 }
 
 const size_t k_max_msg = 4096;
 
-enum {
+enum
+{
     STATE_REQ = 0,
     STATE_RES = 1,
-    STATE_END = 2,  // mark the connection for deletion
+    STATE_END = 2, // mark the connection for deletion
 };
 
-struct Conn {
+struct Conn
+{
     int fd = -1;
-    uint32_t state = 0;     // either STATE_REQ or STATE_RES
+    uint32_t state = 0; // either STATE_REQ or STATE_RES
     // buffer for reading
     size_t rbuf_size = 0;
-    uint8_t rbuf[4 + k_max_msg];
+    uint8_t rbuf[HEADER + k_max_msg];
     // buffer for writing
     size_t wbuf_size = 0;
     size_t wbuf_sent = 0;
-    uint8_t wbuf[4 + k_max_msg];
+    uint8_t wbuf[HEADER + k_max_msg];
 };
 
-static void conn_put(std::vector<Conn *> &fd2conn, struct Conn *conn) {
-    if (fd2conn.size() <= (size_t)conn->fd) {
+static void conn_put(vector<Conn *> &fd2conn, struct Conn *conn)
+{
+    if (fd2conn.size() <= (size_t)conn->fd)
+    {
         fd2conn.resize(conn->fd + 1);
     }
     fd2conn[conn->fd] = conn;
 }
 
-static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd) {
+static int32_t accept_new_conn(vector<Conn *> &fd2conn, int fd)
+{
     // accept
     struct sockaddr_in client_addr = {};
     socklen_t socklen = sizeof(client_addr);
     int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
-    if (connfd < 0) {
+    if (connfd < 0)
+    {
         msg("accept() error");
-        return -1;  // error
+        return -1; // error
     }
 
     // set the new connection fd to nonblocking mode
     fd_set_nb(connfd);
     // creating the struct Conn
     struct Conn *conn = (struct Conn *)malloc(sizeof(struct Conn));
-    if (!conn) {
+    if (!conn)
+    {
         close(connfd);
         return -1;
     }
@@ -97,38 +116,50 @@ static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd) {
 static void state_req(Conn *conn);
 static void state_res(Conn *conn);
 
-static bool try_one_request(Conn *conn) {
+static bool try_one_request(Conn *conn)
+{
     // try to parse a request from the buffer
-    if (conn->rbuf_size < 4) {
+    if (conn->rbuf_size < HEADER)
+    {
         // not enough data in the buffer. Will retry in the next iteration
         return false;
     }
     uint32_t len = 0;
-    memcpy(&len, &conn->rbuf[0], 4);
-    if (len > k_max_msg) {
+    memcpy(&len, &conn->rbuf[0], HEADER);
+    if (len > k_max_msg)
+    {
         msg("too long");
         conn->state = STATE_END;
         return false;
     }
-    if (4 + len > conn->rbuf_size) {
+    if (HEADER + len > conn->rbuf_size)
+    {
         // not enough data in the buffer. Will retry in the next iteration
         return false;
     }
 
     // got one request, do something with it
-    printf("client says: %.*s\n", len, &conn->rbuf[4]);
+    uint32_t reslen = 0;
+    uint32_t resstatus = 0;
+    int32_t error = handle_request(&conn->rbuf[HEADER], len, &resstatus, conn->wbuf, &reslen);
+    if (error)
+    {
+        conn->state = STATE_END;
+        return false;
+    }
 
     // generating echoing response
-    memcpy(&conn->wbuf[0], &len, 4);
-    memcpy(&conn->wbuf[4], &conn->rbuf[4], len);
-    conn->wbuf_size = 4 + len;
+    memcpy(&conn->wbuf[0], &len, HEADER);
+    memcpy(&conn->wbuf[HEADER], &conn->rbuf[HEADER], len);
+    conn->wbuf_size = HEADER + len;
 
     // remove the request from the buffer.
     // note: frequent memmove is inefficient.
     // note: need better handling for production code.
-    size_t remain = conn->rbuf_size - 4 - len;
-    if (remain) {
-        memmove(conn->rbuf, &conn->rbuf[4 + len], remain);
+    size_t remain = conn->rbuf_size - HEADER - len;
+    if (remain)
+    {
+        memmove(conn->rbuf, &conn->rbuf[HEADER + len], remain);
     }
     conn->rbuf_size = remain;
 
@@ -139,28 +170,96 @@ static bool try_one_request(Conn *conn) {
     // continue the outer loop if the request was fully processed
     return (conn->state == STATE_REQ);
 }
+static int32_t handle_request(const uint8_t *req, uint32_t reqlen, uint32_t *rescode, uint8_t *res, uint32_t *reslen)
+{
+    vector<string> cmd;
+    parse_request(req, reqlen, cmd);
 
-static bool try_fill_buffer(Conn *conn) {
+    if (strcasecmp((cmd).at(0), "get"))
+    {
+        *rescode = do_get(cmd, res, reslen);
+    }
+    else if (strcasecmp((cmd).at(0), "set"))
+    {
+        *rescode = do_set(cmd, res, reslen);
+    }
+    else if (strcasecmp((cmd).at(0), "del"))
+    {
+        *rescode = do_del(cmd, res, reslen);
+    }
+}
+
+static uint32_t do_del(vector<string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+}
+static uint32_t do_set(vector<string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+}
+static uint32_t do_get(vector<string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+}
+
+static int8_t parse_request(const uint8_t *req, uint32_t reqlen, vector<string> &cmd)
+{
+    uint8_t error = 0;
+
+    if (reqlen < HEADER)
+    {
+        return -1;
+    }
+    uint32_t nstr = 0;
+    memcpy(&nstr, &req[0], HEADER);
+
+    if (nstr > k_max_msg)
+    {
+        return -2;
+    }
+
+    uint32_t pos = HEADER;
+    while (nstr--)
+    {
+
+        uint32_t str = 0;
+        memcpy(&str, &req[pos], HEADER); // put length of the string in the str variable
+
+        cmd.push_back(string((char *)&req[pos + HEADER], str));
+        pos += HEADER + str; // point pos to next str
+    }
+
+    if (pos != reqlen)
+    {
+        return -3;
+    }
+}
+static bool try_fill_buffer(Conn *conn)
+{
     // try to fill the buffer
     assert(conn->rbuf_size < sizeof(conn->rbuf));
     ssize_t rv = 0;
-    do {
+    do
+    {
         size_t cap = sizeof(conn->rbuf) - conn->rbuf_size;
         rv = read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
     } while (rv < 0 && errno == EINTR);
-    if (rv < 0 && errno == EAGAIN) {
+    if (rv < 0 && errno == EAGAIN)
+    {
         // got EAGAIN, stop.
         return false;
     }
-    if (rv < 0) {
+    if (rv < 0)
+    {
         msg("read() error");
         conn->state = STATE_END;
         return false;
     }
-    if (rv == 0) {
-        if (conn->rbuf_size > 0) {
+    if (rv == 0)
+    {
+        if (conn->rbuf_size > 0)
+        {
             msg("unexpected EOF");
-        } else {
+        }
+        else
+        {
             msg("EOF");
         }
         conn->state = STATE_END;
@@ -172,32 +271,42 @@ static bool try_fill_buffer(Conn *conn) {
 
     // Try to process requests one by one.
     // Why is there a loop? Please read the explanation of "pipelining".
-    while (try_one_request(conn)) {}
+    while (try_one_request(conn))
+    {
+    }
     return (conn->state == STATE_REQ);
 }
 
-static void state_req(Conn *conn) {
-    while (try_fill_buffer(conn)) {}
+static void state_req(Conn *conn)
+{
+    while (try_fill_buffer(conn))
+    {
+    }
 }
 
-static bool try_flush_buffer(Conn *conn) {
+static bool try_flush_buffer(Conn *conn)
+{
     ssize_t rv = 0;
-    do {
+    do
+    {
         size_t remain = conn->wbuf_size - conn->wbuf_sent;
         rv = write(conn->fd, &conn->wbuf[conn->wbuf_sent], remain);
     } while (rv < 0 && errno == EINTR);
-    if (rv < 0 && errno == EAGAIN) {
+    if (rv < 0 && errno == EAGAIN)
+    {
         // got EAGAIN, stop.
         return false;
     }
-    if (rv < 0) {
+    if (rv < 0)
+    {
         msg("write() error");
         conn->state = STATE_END;
         return false;
     }
     conn->wbuf_sent += (size_t)rv;
     assert(conn->wbuf_sent <= conn->wbuf_size);
-    if (conn->wbuf_sent == conn->wbuf_size) {
+    if (conn->wbuf_sent == conn->wbuf_size)
+    {
         // response was fully sent, change state back
         conn->state = STATE_REQ;
         conn->wbuf_sent = 0;
@@ -208,23 +317,34 @@ static bool try_flush_buffer(Conn *conn) {
     return true;
 }
 
-static void state_res(Conn *conn) {
-    while (try_flush_buffer(conn)) {}
-}
-
-static void connection_io(Conn *conn) {
-    if (conn->state == STATE_REQ) {
-        state_req(conn);
-    } else if (conn->state == STATE_RES) {
-        state_res(conn);
-    } else {
-        assert(0);  // not expected
+static void state_res(Conn *conn)
+{
+    while (try_flush_buffer(conn))
+    {
     }
 }
 
-int main() {
+static void connection_io(Conn *conn)
+{
+    if (conn->state == STATE_REQ)
+    {
+        state_req(conn);
+    }
+    else if (conn->state == STATE_RES)
+    {
+        state_res(conn);
+    }
+    else
+    {
+        assert(0); // not expected
+    }
+}
+
+int main()
+{
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
+    if (fd < 0)
+    {
         die("socket()");
     }
 
@@ -235,35 +355,40 @@ int main() {
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    addr.sin_addr.s_addr = ntohl(0); // wildcard address 0.0.0.0
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
-    if (rv) {
+    if (rv)
+    {
         die("bind()");
     }
 
     // listen
     rv = listen(fd, SOMAXCONN);
-    if (rv) {
+    if (rv)
+    {
         die("listen()");
     }
 
     // a map of all client connections, keyed by fd
-    std::vector<Conn *> fd2conn;
+    vector<Conn *> fd2conn;
 
     // set the listen fd to nonblocking mode
     fd_set_nb(fd);
 
     // the event loop
-    std::vector<struct pollfd> poll_args;
-    while (true) {
+    vector<struct pollfd> poll_args;
+    while (true)
+    {
         // prepare the arguments of the poll()
         poll_args.clear();
         // for convenience, the listening fd is put in the first position
         struct pollfd pfd = {fd, POLLIN, 0};
         poll_args.push_back(pfd);
         // connection fds
-        for (Conn *conn : fd2conn) {
-            if (!conn) {
+        for (Conn *conn : fd2conn)
+        {
+            if (!conn)
+            {
                 continue;
             }
             struct pollfd pfd = {};
@@ -276,16 +401,20 @@ int main() {
         // poll for active fds
         // the timeout argument doesn't matter here
         int rv = poll(poll_args.data(), (nfds_t)poll_args.size(), 1000);
-        if (rv < 0) {
+        if (rv < 0)
+        {
             die("poll");
         }
 
         // process active connections
-        for (size_t i = 1; i < poll_args.size(); ++i) {
-            if (poll_args[i].revents) {
+        for (size_t i = 1; i < poll_args.size(); ++i)
+        {
+            if (poll_args[i].revents)
+            {
                 Conn *conn = fd2conn[poll_args[i].fd];
                 connection_io(conn);
-                if (conn->state == STATE_END) {
+                if (conn->state == STATE_END)
+                {
                     // client closed normally, or something bad happened.
                     // destroy this connection
                     fd2conn[conn->fd] = NULL;
@@ -296,7 +425,8 @@ int main() {
         }
 
         // try to accept a new connection if the listening fd is active
-        if (poll_args[0].revents) {
+        if (poll_args[0].revents)
+        {
             (void)accept_new_conn(fd2conn, fd);
         }
     }
