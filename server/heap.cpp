81f5ac8ed8b764719c753af03372dfa9fa93971e
ZeyadAttia5 <85710632+ZeyadAttia5@users.noreply.github.com>
2023-09-03 01:56:13 +0300

    added heapifyUp/Down, fixed a few typos
    TODO: add a test for heapifyUp/Down

--- server/heap.cpp
+++ server/heap.cpp
@@ -1,46 +1,17 @@
 #include "heap.hpp"
 #include <iostream>
 
-size_t Heap::heap_getParentIndex(size_t i)
-{
-    return (i + 1) / 2 - 1;
-}
+size_t Heap::heap_getParentIndex(size_t i)  { return (i + 1) / 2 - 1; }
+size_t Heap::heap_getLeftIndex(size_t i)    { return i * 2 + 1; }
+size_t Heap::heap_getRightIndex(size_t i)   { return i * 2 + 2; }
 
-size_t Heap::heap_getLeftIndex(size_t i)
-{
-    return i * 2 + 1;
-}
+bool Heap::heap_hasParent(size_t i) { return heap_getParentIndex(i) >= 0; }
+bool Heap::heap_hasLeft(size_t i)   { return heap_getLeftIndex(i) >= 0; }
+bool Heap::heap_hasRight(size_t i)  { return heap_getRightIndex(i) >= 0; }
 
-size_t Heap::heap_getRightIndex(size_t i)
-{
-    return i * 2 + 2;
-}
-
-bool Heap::heap_hasParent(size_t i)
-{
-    return heap_getParentIndex(i) >= 0;
-}
-bool Heap::heap_hasLeft(size_t i)
-{
-    return heap_getLeftIndex(i) >= 0;
-}
-bool Heap::heap_hasRight(size_t i)
-{
-    return heap_getRightIndex(i) >= 0;
-}
-
-uint64_t Heap::heap_getParentIndex(size_t i)
-{
-    return heap[heap_getParentIndex(i)];
-}
-uint64_t Heap::heap_getLeftIndex(size_t i)
-{
-    return heap[heap_getLeftIndex(i)];
-}
-uint64_t Heap::heap_getRightIndex(size_t i)
-{
-    return heap[heap_getRightIndex(i)];
-}
+uint64_t Heap::heap_getParent(size_t i) { return (heap_hasParent(i)) ? heap[heap_getParentIndex(i)] : 0; }
+uint64_t Heap::heap_getLeft(size_t i)   { return (heap_hasLeft) ? heap[heap_getLeftIndex(i)] : 0; }
+uint64_t Heap::heap_getRight(size_t i)  { return (heap_hasRight) ? heap[heap_getRightIndex(i)] : 0; }
 
 void Heap::swap(size_t i, size_t j)
 {
@@ -54,7 +25,7 @@ void Heap::swap(size_t i, size_t j)
     }
 }
 
-//peek at the top of the heap (the smallest element)
+// peek at the top of the heap (the smallest element)
 uint64_t Heap::peek()
 {
     if (heap.size() == 0)
@@ -65,7 +36,7 @@ uint64_t Heap::peek()
     return heap[0];
 }
 
-//poll the top of the heap (the smallest element)
+// poll the top of the heap (the smallest element)
 uint64_t Heap::poll()
 {
     if (heap.size() == 0)
@@ -76,12 +47,47 @@ uint64_t Heap::poll()
     uint64_t item = heap[0];
     heap[0] = heap[heap.size() - 1];
     heap.pop_back();
-    heapifyDown();
+    heap_heapifyDown();
     return item;
 }
 
-//add an element to the heap
-void Heap::add(uint64_t value){
+// add an element to the heap
+void Heap::add(uint64_t value)
+{
     heap.push_back(value);
-    heapifyUp();
+    heap_heapifyUp();
+}
+
+// heapify up
+void Heap::heap_heapifyUp()
+{
+    size_t index = heap.size() - 1;
+    while (heap_hasParent(index) && heap_getParentIndex(index) > heap[index])
+    {
+        swap(heap_getParentIndex(index), index);
+        index = heap_getParentIndex(index);
+    }
+}
+
+// heapify down
+void Heap::heap_heapifyDown()
+{
+    size_t index = 0;
+    while (heap_hasLeft(index))
+    {
+        size_t smallerChildIndex = heap_getLeftIndex(index);
+        if (heap_hasRight(index) && heap_getRightIndex(index) < heap_getLeftIndex(index))
+        {
+            smallerChildIndex = heap_getRightIndex(index);
+        }
+        if (heap[index] < heap[smallerChildIndex])
+        {
+            break;
+        }
+        else
+        {
+            swap(index, smallerChildIndex);
+        }
+        index = smallerChildIndex;
+    }
 }
--- server/heap.hpp
+++ server/heap.hpp
@@ -8,9 +8,9 @@ class Heap
 private:
     // heap is a vector of uint64_t which are timers :)
     std::vector<uint64_t> heap;
+
     void swap(size_t i, size_t j);
 
-public:
     size_t heap_getParentIndex(size_t i);
     size_t heap_getLeftIndex(size_t i);
     size_t heap_getRightIndex(size_t i);
@@ -19,10 +19,14 @@ public:
     bool heap_hasLeft(size_t i);
     bool heap_hasRight(size_t i);
 
-    uint64_t heap_getParentIndex(size_t i);
-    uint64_t heap_getLeftIndex(size_t i);
-    uint64_t heap_getRightIndex(size_t i);
+    uint64_t heap_getParent(size_t i);
+    uint64_t heap_getLeft(size_t i);
+    uint64_t heap_getRight(size_t i);
 
+    void heap_heapifyUp();
+    void heap_heapifyDown();
+
+public:
     uint64_t peek();
     uint64_t poll();
     void add(uint64_t value);
