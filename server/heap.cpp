841962c35d97d4713301d7f5d9c0a74a6d0ad0cc
ZeyadAttia5 <85710632+ZeyadAttia5@users.noreply.github.com>
2023-09-03 02:52:34 +0300

    Fixed heap size update bug

--- server/heap.cpp
+++ server/heap.cpp
@@ -1,27 +1,39 @@
 #include "heap.hpp"
 #include <iostream>
 
-size_t Heap::heap_getParentIndex(size_t i)  { return (i + 1) / 2 - 1; }
-size_t Heap::heap_getLeftIndex(size_t i)    { return i * 2 + 1; }
-size_t Heap::heap_getRightIndex(size_t i)   { return i * 2 + 2; }
+int64_t Heap::getParentIndex(size_t i)
+{
+    if (i == 0)
+    {
+        return -1;
+    }
+    int64_t idx = (i - 1) / 2;
+    return idx;
+}
+int64_t Heap::getLeftIndex(size_t i) { return i * 2 + 1; }
+int64_t Heap::getRightIndex(size_t i) { return i * 2 + 2; }
 
-bool Heap::heap_hasParent(size_t i) { return heap_getParentIndex(i) >= 0; }
-bool Heap::heap_hasLeft(size_t i)   { return heap_getLeftIndex(i) >= 0; }
-bool Heap::heap_hasRight(size_t i)  { return heap_getRightIndex(i) >= 0; }
+bool Heap::hasParent(size_t i)
+{
+    bool parentStat = getParentIndex(i) >= 0;
+    return parentStat;
+}
+bool Heap::hasLeft(size_t i) { return getLeftIndex(i) < heap.size(); }
+bool Heap::hasRight(size_t i) { return getRightIndex(i) < heap.size(); }
 
-uint64_t Heap::heap_getParent(size_t i) { return (heap_hasParent(i)) ? heap[heap_getParentIndex(i)] : 0; }
-uint64_t Heap::heap_getLeft(size_t i)   { return (heap_hasLeft) ? heap[heap_getLeftIndex(i)] : 0; }
-uint64_t Heap::heap_getRight(size_t i)  { return (heap_hasRight) ? heap[heap_getRightIndex(i)] : 0; }
+uint64_t Heap::getParent(size_t i) { return (hasParent(i)) ? heap[getParentIndex(i)] : 0; }
+uint64_t Heap::getLeft(size_t i) { return (hasLeft(i)) ? heap[getLeftIndex(i)] : 0; }
+uint64_t Heap::getRight(size_t i) { return (hasRight(i)) ? heap[getRightIndex(i)] : 0; }
 
+bool Heap::isEmpty() { return heap.size() == 0; }
+size_t Heap::size() { return heap.size(); }
+
+// swap two elements in the heap
 void Heap::swap(size_t i, size_t j)
 {
     if (i < heap.size() && j < heap.size())
     {
         std::swap(heap[i], heap[j]);
-    }
-    else
-    {
-        std::cerr << "Index out of range." << std::endl;
     }
 }
 
@@ -47,7 +59,7 @@ uint64_t Heap::poll()
     uint64_t item = heap[0];
     heap[0] = heap[heap.size() - 1];
     heap.pop_back();
-    heap_heapifyDown();
+    heapifyDown();
     return item;
 }
 
@@ -55,30 +67,39 @@ uint64_t Heap::poll()
 void Heap::add(uint64_t value)
 {
     heap.push_back(value);
-    heap_heapifyUp();
+    heapifyUp();
 }
 
 // heapify up
-void Heap::heap_heapifyUp()
+void Heap::heapifyUp()
 {
-    size_t index = heap.size() - 1;
-    while (heap_hasParent(index) && heap_getParentIndex(index) > heap[index])
+    size_t index;
+    if (this->isEmpty())
     {
-        swap(heap_getParentIndex(index), index);
-        index = heap_getParentIndex(index);
+        index = 0;
+    }
+    else
+    {
+
+        index = heap.size() - 1;
+    }
+    while (hasParent(index) && getParent(index) > heap[index])
+    {
+        swap(getParentIndex(index), index);
+        index = getParentIndex(index);
     }
 }
 
 // heapify down
-void Heap::heap_heapifyDown()
+void Heap::heapifyDown()
 {
     size_t index = 0;
-    while (heap_hasLeft(index))
+    while (hasLeft(index))
     {
-        size_t smallerChildIndex = heap_getLeftIndex(index);
-        if (heap_hasRight(index) && heap_getRightIndex(index) < heap_getLeftIndex(index))
+        size_t smallerChildIndex = getLeftIndex(index);
+        if (hasRight(index) && getRight(index) < getLeft(index))
         {
-            smallerChildIndex = heap_getRightIndex(index);
+            smallerChildIndex = getRightIndex(index);
         }
         if (heap[index] < heap[smallerChildIndex])
         {
@@ -89,5 +110,48 @@ void Heap::heap_heapifyDown()
             swap(index, smallerChildIndex);
         }
         index = smallerChildIndex;
+    }
+}
+
+// print the heap
+void Heap::heap_print()
+{
+    for (size_t i = 0; i < heap.size(); i++)
+    {
+        if (hasLeft(i))
+        {
+            std::cout << " PARENT : " << heap[i] << " LEFT CHILD : " << heap[getLeftIndex(i)];
+        }
+        if (hasRight(i))
+        {
+            std::cout << " RIGHT CHILD :" << heap[getRightIndex(i)] << std::endl;
+        }
+        else
+        {
+            std::cout << std::endl;
+        }
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    Heap *heap = new Heap();
+
+    heap->add(5);
+    heap->add(3);
+    heap->add(4);
+    heap->add(1);
+    heap->add(2);
+    heap->add(0);
+    std::cout << heap->peek() << std::endl;
+
+    heap->heap_print();
+
+    // tests heapifyDown/poll
+    for (size_t i = 0; i < 6; i++)
+    {
+        // std::cout << heap->peek() << std::endl;
+        std::cout << heap->poll() << std::endl;
     }
 }
--- server/heap.hpp
+++ server/heap.hpp
@@ -11,23 +11,32 @@ private:
 
     void swap(size_t i, size_t j);
 
-    size_t heap_getParentIndex(size_t i);
-    size_t heap_getLeftIndex(size_t i);
-    size_t heap_getRightIndex(size_t i);
+    int64_t getParentIndex(size_t i);
+    int64_t getLeftIndex(size_t i);
+    int64_t getRightIndex(size_t i);
 
-    bool heap_hasParent(size_t i);
-    bool heap_hasLeft(size_t i);
-    bool heap_hasRight(size_t i);
+    bool hasParent(size_t i);
+    bool hasLeft(size_t i);
+    bool hasRight(size_t i);
 
-    uint64_t heap_getParent(size_t i);
-    uint64_t heap_getLeft(size_t i);
-    uint64_t heap_getRight(size_t i);
+    uint64_t getParent(size_t i);
+    uint64_t getLeft(size_t i);
+    uint64_t getRight(size_t i);
 
-    void heap_heapifyUp();
-    void heap_heapifyDown();
+    void heapifyUp();
+    void heapifyDown();
 
 public:
     uint64_t peek();
     uint64_t poll();
     void add(uint64_t value);
+    void heap_print();
+    bool isEmpty();
+    size_t size();
+    Heap()
+    {
+        // Initialize the heap as an empty vector
+        std::vector<uint64_t> emptyHeap;
+        heap = emptyHeap;
+    }
 };
